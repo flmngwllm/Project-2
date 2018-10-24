30af498558d6efd9da2e18d02ae19b9728aaa778
CB_ATL_117_William_Fleming <cb_atl_117_william_fleming@CBs-MacBook-Air.local>
2018-10-24 18:23:48 -0400

    finished setup

---
+++ db/connection.js
@@ -0,0 +1,8 @@
+const env = require('dotenv').config()
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGODB_URI).then(() => {
+console.log('Connected to MongoDB')
+})
+
+module.exports = mongoose
