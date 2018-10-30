6f6fd49bb0b734acb126fd9dc511e992d63c8844
CB_ATL_117_William_Fleming <cb_atl_117_william_fleming@CBs-MacBook-Air.local>
2018-10-30 10:30:16 -0400

    changed seeds file

--- db/seeds.js
+++ db/seeds.js
@@ -1,4 +1,46 @@
 const Tournaments = require('../models/Tournaments')
+const Thread = require('../models/Thread')
+const Posts = require('../models/posts')
+// const User = require('..models/User')
+const mongoose = require('./connection')
+
+const canadaPo = new Posts ({
+    post:"I missed it too",
+    
+
+})
+
+const usPo = new Posts ({
+    post:"You should get better",
+    
+
+})
+
+const euPo = new Posts ({
+    post:"It went out for me too",
+    
+
+})
+
+
+
+const canadaTh = new Thread ({
+    title:"Did any one catch the finals",
+    posts:[canadaPo]
+
+})
+
+const UsTh = new Thread ({
+    title:"Can't believe I lost again",
+    posts:[usPo]
+
+})
+
+const EUTh = new Thread ({
+    title:"What happened to the stream",
+    posts:[euPo]
+
+})
 
 let newTournaments = [
     {
@@ -31,3 +73,17 @@ let newTournaments = [
 Tournaments.create(newTournaments).then(tournaments =>{
     console.log('Saved Tournaments', tournaments)
 })
+
+// Tournaments.remove()
+// .then(() => console.log("Database seeded success"))
+// .then(() => mongoose.connection.close())
+
+Thread.remove({})
+// inserts products into kroger, target, etc..
+    .then(() => Posts.insertMany([canadaPo, euPo, usPo]))
+    .then(() => canadaTh.save())
+    .then(() => EUTh.save())
+    .then(() => UsTh.save())
+    .then(() => console.log("Database seeded success"))
+    // to close out seed.js
+    .then(() => mongoose.connection.close())
